e4f937ccc0400f502a08341e243b4f36198e6a41
LadyVamp <ladyvamp666@gmail.com>
2016-01-21 00:25:44 +0200

    parallax test

--- js/parallax.js
+++ js/parallax.js
@@ -1,4 +1,14 @@
-$.stellar({
-    horizontalScrolling: false,
-    responsive: true
-});
+$(window).scroll(function() {
+	var st = $(this).scrollTop();
+
+	// // Parallax для текста
+	// $(".header_text").css({
+	// 	"transform" : "translate(0%, "+ st + "%"
+	// });
+
+	// Parallax для картинки
+	$(".sect_2 img").css({
+		"transform" : "translate(0%, "+ st /20+ "%"
+	});
+
+}); 
